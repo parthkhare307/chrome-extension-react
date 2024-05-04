5a531acbf62f0e1d8cda759f1885e0ef6236fea8
Parth Khare <parthkhare307@gmail.com>
2024-05-05 04:03:59 +0530

    Added Alert message by clicking in PopUp

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import { useState } from 'react'
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const onclick = async()=>{
+      let[tab] = await chrome.tabs.query({active:true});
+      chrome.scripting.executeScript({
+        target:{ tabId : tab.id!},
+        func: ()=>{
+            alert("Hello from my extension!");
+        }
+      });
+  }
 
   return (
     <>
@@ -16,10 +25,10 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>My Extension</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={onclick}>
+          Click Here
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
